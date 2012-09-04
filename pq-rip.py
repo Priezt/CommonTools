0ac8dfeffe8dcedb4ce3f63c4f0535b446e4e865
Priezt <vegetabird@gmail.com>
2012-09-04 10:10:16 +0800

    nfc

--- pq-rip.py
+++ pq-rip.py
@@ -6,19 +6,22 @@ import sys
 import getopt
 from urlparse import urljoin
 
-opts, args = getopt.getopt(sys.argv[1:], "ur")
+opts, args = getopt.getopt(sys.argv[1:], "ura:")
 
 if len(args) < 2:
 	raise Exception('Arguments not enough')
 
 need_url = False
 need_reverse = False
+need_attribute = False
 
 for o, a in opts:
 	if o == "-u":
 		need_url = True
 	if o == "-r":
 		need_reverse = True
+	if o == "-a":
+		need_attribute = a
 
 url = args[0]
 selector = args[1]
@@ -28,15 +31,18 @@ doc = pq(url)
 result = []
 
 def cb(idx, node):
-	txt = pq(node).text().encode('utf-8')
-	txt = re.sub(r'[\r\n]', '', txt)
-	if need_url:
-		href = pq(node).attr('href')
-		if href:
-			href = urljoin(url, href)
-			txt = href + "|" + txt
-		else:
-			txt = url + "|" + txt
+	if need_attribute:
+		txt = pq(node).attr(need_attribute)
+	else:
+		txt = pq(node).text().encode('utf-8')
+		txt = re.sub(r'[\r\n]', '', txt)
+		if need_url:
+			href = pq(node).attr('href')
+			if href:
+				href = urljoin(url, href)
+				txt = href + "|" + txt
+			else:
+				txt = url + "|" + txt
 	result.append(txt)
 
 items = doc(selector)
