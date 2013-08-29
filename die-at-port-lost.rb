eae31d07a54bfe41d8991b6c93d514a2292e7043
Priezt <vegetabird@gmail.com>
2013-08-29 23:11:28 +0800

    print message for die-at-port-lost

--- die-at-port-lost.rb
+++ die-at-port-lost.rb
@@ -3,6 +3,8 @@
 port = ARGV.shift or throw "need port"
 interval = (ARGV.shift or '60').to_i
 
+puts "die until port(#{port}) lost, interval(#{interval})"
+
 loop do
 	sleep interval
 	if `netstat -an | grep #{port} | wc -l`.to_i == 0
