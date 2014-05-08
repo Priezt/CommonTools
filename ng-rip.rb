05e0126b2bed9a0fa8506b791403889d74564ae8
Priezt <vegetabird@gmail.com>
2014-05-08 02:47:26 -0400

    add parse file function to ng-rip

--- ng-rip.rb
+++ ng-rip.rb
@@ -2,11 +2,25 @@
 
 require 'open-uri'
 require 'nokogiri'
+require 'uri'
+require 'optparse'
+
+params = ARGV.getopts("u:")
 
 url = ARGV[0]
 selector = ARGV[1]
 
-page = Nokogiri::HTML(open(url))
+if url =~ /^http/
+	page = Nokogiri::HTML(open(url))
+else
+	page = Nokogiri::HTML(File.open(url))
+	url = params["u"] || raise("need -u")
+end
+
 page.css(selector).each do |a|
-	puts "#{url}|#{a.text}".gsub /\s+/, ' '
+	if a.name == "a"
+		puts "#{URI.join url, a.attr("href")}|#{a.text}".gsub /\s+/, ' '
+	else
+		puts "#{url}|#{a.text}".gsub /\s+/, ' '
+	end
 end
